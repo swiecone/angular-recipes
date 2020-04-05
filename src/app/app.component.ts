4a8a05378b565d839b5907f63ba2308b7b50432e
Usuario <usuario@MacBook-Air-de-Alex.local>
2020-04-05 15:53:50 +0200

    vuelve a ser recipe la home de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'recipes';
-  loadedFeature = 'shopping-list';
+  loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
